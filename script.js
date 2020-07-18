860cc9ce1a5359422690db9ba7915b7d5fda6d40
thashu0424 <52147960+thashu0424@users.noreply.github.com>
2020-07-18 18:11:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function makeTimer() {
 
-    var endTime = new Date("August 26, 2020 18:00:00 ");
+    var endTime = new Date("August 26, 2020 18:00:00 UTC+05:30 ");
     var endTime = (Date.parse(endTime)) / 1000;
 
     var now = new Date();
